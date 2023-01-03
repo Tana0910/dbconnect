acadd66fa677f84245a8a1d7ef09f809e593e162
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2023-01-03 22:38:35 +0900

    add C# program
     On branch master
     Changes to be committed:
    	new file:   csharp/App/App.sln
    	new file:   csharp/App/App/App.csproj
    	new file:   csharp/App/App/DBconnect.cs
    	new file:   csharp/App/App/Program.cs

---
+++ csharp/App/App/DBconnect.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    [JsonObject("DBConnect")]
+    class DBconnect
+    {
+        [JsonProperty("server")]
+        public string Server { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("database")]
+        public string Database { get; set; }
+    }
+}
---
+++ csharp/App/App/Program.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+JsonSerializer jsonSerializer = new JsonSerializer();
+App.DBconnect? dbc = new App.DBconnect();
+
+var filepath = @"path\to\file"; // type the path to the json file.
+using (StreamReader sr = new StreamReader($"{filepath}"))
+using (JsonReader reader = new JsonTextReader(sr))
+{
+    dbc = jsonSerializer.Deserialize<App.DBconnect>(reader);
+}
+if (dbc == null)
+{
+    Console.WriteLine("Failed to read ...");
+    return;
+}
+
+await using var conn = new NpgsqlConnection($"Host={dbc.Server};Username={dbc.Username};Password={dbc.Password};Database={dbc.Database}");
+await conn.OpenAsync();
+
+var tablename = @"tablename"; // type your table name.
+await using (var cmd = new NpgsqlCommand($"SELECT * FROM {tablename}", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(reader.GetString(0) + ", " + reader.GetString(1));
+    }
+}
